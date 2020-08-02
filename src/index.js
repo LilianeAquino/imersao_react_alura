6d3ce90727ec52cb458abe869c720e202ff004ab
Liliane de Aquino <lilianelou@hotmail.com>
2020-08-02 10:14:51 -0300

    Aula 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './index.css';
-import App from './App';
+
+import Home from './pages/home/Home';
+import CadastroVideo from './pages/cadastro/video/CadastroVideo.js';
+import CadastroCategoria from './pages/cadastro/Categoria/CategoriaVideo';
+import Page404 from './pages/404/Page404.js';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={Home} exact/>
+      <Route path='/cadastro/video' component={CadastroVideo} />
+      <Route path='/cadastro/categoria' component={CadastroCategoria} />
+      <Route component={Page404}/>
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
