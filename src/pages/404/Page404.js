ee725ed4f6b4c1fdd0c125ff3555d7ae470d53da
Liliane de Aquino <lilianelou@hotmail.com>
2020-08-02 11:15:39 -0300

    Aula 2

--- src/pages/404/Page404.js
+++ src/pages/404/Page404.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import PageDefault from '../../components/PageDefault';
+import { Link } from 'react-router-dom';
+
+import page404 from '../../assets/page404.gif';
+
 
 function Page404(){
     return (
-        <div>
-            Página 404
-        </div>
+        <PageDefault>
+            <Link to="/">
+                <image404>
+                    <img className="Page404" src={ page404 } width="500" height="auto" alt='Gif página 404'
+                         style={{position:'absolute', left: '400px', top: '90px'}}/>
+                </image404>
+            </Link>
+        </PageDefault>
     );
 }
 
--- src/pages/cadastro/categoria/CategoriaVideo.js
+++ src/pages/cadastro/categoria/CategoriaVideo.js
@@ -7,6 +7,18 @@ function CadastroCategoria(){
     return (
     <PageDefault>
         <h1>Cadastro de Categoria</h1>
+        <form>
+            <label>
+                Nome categoria: <input type='text'/>
+            </label>
+
+            <button>Cadastrar</button>
+
+        </form>
+
+
+
+
 
         <Link to='/'>
             Ir para a home
